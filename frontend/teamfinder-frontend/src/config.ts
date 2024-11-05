de2a65c0957a723de2965265ef80ce89ef2977c5
Kaveesh Khattar <kaveeshkhattar@gmail.com>
2024-11-05 21:19:13 +0530

    Updated endpoints

--- frontend/teamfinder-frontend/src/config.ts
+++ frontend/teamfinder-frontend/src/config.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://teamfinder-production.up.railway.app';
-// export const BASE_URL = 'https://teamfinder-wpal.onrender.com';
+// export const BASE_URL = 'https://teamfinder-production.up.railway.app';
+export const BASE_URL = 'https://teamfinder-wpal.onrender.com';
 // export const BASE_URL = 'http://localhost:8080';
